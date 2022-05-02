90b49c12b3fc481d955f1f5a9e3a73c6591bf326
Andrea Ji <andrea.hb.ji@outlook.com>
2022-05-02 14:24:52 +0800

    实现光照类

--- includes/lighting.h
+++ includes/lighting.h
@@ -3,44 +3,150 @@
 
 #include <learnopengl/shader.h>
 
+class Material
+{
+public:
+    /**
+     * @brief 使用光照贴图时使用此构造函数创建 Material 对象
+     * 
+     * @param shininess 光泽度
+     */
+    Material(float shininess);
 
+    /**
+     * @brief 属性完整的 Material 对象
+     * 
+     * @param ambient 环境光反射率
+     * @param diffuse 漫反射率
+     * @param specular 镜面光反射率
+     * @param shininess 光泽度
+     */
+    Material(glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, float shininess);
 
+    /**
+     * @brief 应用 Material 属性
+     * 
+     * @param shader 
+     */
+    void applyMaterial(Shader* shader);
+
+    /**
+     * @brief 为光照贴图应用 Material 属性
+     * 
+     * @param shader 
+     */
+    void applyLightingMapMaterial(Shader* shader);
+protected:
+    glm::vec3 ambient;
+    glm::vec3 diffuse;
+    glm::vec3 specular;
+    float shininess;
+};
+
+/**
+ * @brief 基础光照
+ * 
+ */
 class Lighting
 {
 public:
-    void initDirectionalLight(Shader* shader, glm::vec3 viewPos, glm::vec3 lightDirection, glm::vec3 lightAmbient, glm::vec3 lightDiffuse, glm::vec3 lightSpecular)
-    {
-        shader->setVec3("viewPos", viewPos);
-        shader->setVec3("dirLight.direction", lightDirection);
-        shader->setVec3("dirLight.ambient", lightAmbient);
-        shader->setVec3("dirLight.diffuse", lightDiffuse);
-        shader->setVec3("dirLight.specular", lightSpecular);
-    }
-    void initPointLight(Shader* shader, unsigned int lightID, glm::vec3 viewPos, glm::vec3 lightPos, glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, float constant, float linear, float quadratic)
-    {
-        shader->setVec3("viewPos", viewPos);
-        shader->setVec3("pointLights[" + std::to_string(lightID) + "].position", lightPos);
-        shader->setVec3("pointLights[" + std::to_string(lightID) + "].ambient", ambient);
-        shader->setVec3("pointLights[" + std::to_string(lightID) + "].diffuse", diffuse);
-        shader->setVec3("pointLights[" + std::to_string(lightID) + "].specular", specular);
-        shader->setFloat("pointLights[" + std::to_string(lightID) + "].constant", constant);
-        shader->setFloat("pointLights[" + std::to_string(lightID) + "].linear", linear);
-        shader->setFloat("pointLights[" + std::to_string(lightID) + "].quadratic", quadratic);
-    }
-    void initSpotLight(Shader* shader, unsigned int lightID, glm::vec3 viewPos, glm::vec3 lightPos, glm::vec3 lightDirection, glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, float constant, float linear, float quadratic, float cutOff, float outerCutOff)
-    {
-        shader->setVec3("viewPos", viewPos);
-        shader->setVec3("spotLights[" + std::to_string(lightID) + "].position", lightPos);
-        shader->setVec3("spotLights[" + std::to_string(lightID) + "].direction", lightDirection);
-        shader->setVec3("spotLights[" + std::to_string(lightID) + "].ambient", ambient);
-        shader->setVec3("spotLights[" + std::to_string(lightID) + "].diffuse", diffuse);
-        shader->setVec3("spotLights[" + std::to_string(lightID) + "].specular", specular);
-        shader->setFloat("spotLights[" + std::to_string(lightID) + "].constant", constant);
-        shader->setFloat("spotLights[" + std::to_string(lightID) + "].linear", linear);
-        shader->setFloat("spotLights[" + std::to_string(lightID) + "].quadratic", quadratic);
-        shader->setFloat("spotLights[" + std::to_string(lightID) + "].cutOff", cutOff);
-        shader->setFloat("spotLights[" + std::to_string(lightID) + "].outerCutOff", outerCutOff);
-    }
+    Lighting(glm::vec3 viewPos, glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular);
+    /**
+     * @brief 应用基础光照
+     * 
+     * @param shader 着色器
+     * @param lightPos 光源位置
+     */
+    void apply(Shader* shader, glm::vec3 lightPos);
+protected:
+    glm::vec3 viewPos;
+    glm::vec3 ambient;
+    glm::vec3 diffuse;
+    glm::vec3 specular;
 };
 
-#endif
+/**
+ * @brief 平行光
+ * 
+ */
+class DirectionalLight: protected Lighting
+{
+public:
+    /**
+     * @brief 创建平行光
+     * 
+     * @param viewPos 观察位置 
+     * @param ambient 环境光反射率
+     * @param diffuse 漫反射率
+     * @param specular 镜面光反射率
+     */
+    DirectionalLight(glm::vec3 viewPos, glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular);
+    /**
+     * @brief 应用平行光
+     * 
+     * @param shader 着色器
+     * @param lightDirection 片段到光源的光线方向（一般为全局光照方向向量取反）
+     */
+    void apply(Shader* shader, glm::vec3 lightDirection);
+protected:
+};
+
+/**
+ * @brief 点光源
+ * 
+ */
+class PointLight : protected Lighting
+{
+public:
+    /**
+     * @brief 创建点光源
+     * 
+     * @param viewPos 观察位置 
+     * @param ambient 环境光反射率
+     * @param diffuse 漫反射率
+     * @param specular 镜面光反射率
+     * @param constant 常量衰减参数
+     * @param linear 线性衰减参数
+     * @param quadratic 二次衰减参数
+     */
+    PointLight(glm::vec3 viewPos, glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, float constant, float linear, float quadratic);
+    void apply(Shader* shader, unsigned int lightID, glm::vec3 lightPos);
+protected:
+    float constant;
+    float linear;
+    float quadratic;
+};
+
+/**
+ * @brief 聚光光源
+ * 
+ */
+class SpotLight : protected Lighting
+{
+public:
+    /**
+     * @brief 创建聚光光源
+     * 
+     * @param viewPos 观察位置
+     * @param lightDirection 片段到光源的光线方向（一般为全局光照方向向量取反）
+     * @param ambient 环境光反射率
+     * @param diffuse 漫反射率
+     * @param specular 镜面光反射率
+     * @param constant 常量衰减参数
+     * @param linear 线性衰减参数
+     * @param quadratic 二次衰减参数
+     * @param cutOff 内圆锥角度
+     * @param outerCutOff 外圆锥角度
+     */
+    SpotLight(glm::vec3 viewPos, glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, float constant, float linear, float quadratic, float cutOff, float outerCutOff);
+    void apply(Shader* shader, unsigned int lightID, glm::vec3 lightPos, glm::vec3 lightDirection);
+protected:
+    glm::vec3 lightDirection;
+    float constant;
+    float linear;
+    float quadratic;
+    float cutOff;
+    float outerCutOff;
+};
+
+#endif // LIGHTING_H
---
+++ src/lighting.cpp
@@ -0,0 +1,90 @@
+#include <lighting.h>
+
+Material::Material(float shininess):shininess(shininess)
+{
+}
+
+void Material::applyLightingMapMaterial(Shader* shader)
+{
+    shader->setFloat("material.shininess", shininess);
+}
+
+Material::Material(glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, float shininess)
+    :ambient(ambient),diffuse(diffuse), specular(specular), shininess(shininess)
+{
+}
+
+void Material::applyMaterial(Shader* shader)
+{
+    shader->setVec3("material.ambient", ambient);
+    shader->setVec3("material.diffuse", diffuse);
+    shader->setVec3("material.specular", specular);
+    shader->setFloat("material.shininess", shininess);
+}
+
+Lighting::Lighting(glm::vec3 viewPos, glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular)
+    :viewPos(viewPos),ambient(ambient),diffuse(diffuse), specular(specular)
+{ 
+}
+
+void Lighting::apply(Shader* shader, glm::vec3 lightPos)
+{
+    shader->setVec3("viewPos", viewPos);
+    shader->setVec3("light.position", lightPos);
+    shader->setVec3("light.ambient", ambient);
+    shader->setVec3("light.diffuse", diffuse);
+    shader->setVec3("light.specular", specular);
+}
+
+DirectionalLight::DirectionalLight(glm::vec3 viewPos, glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular)
+    :Lighting(viewPos, ambient, diffuse, specular)
+{
+}
+
+void DirectionalLight::apply(Shader* shader, glm::vec3 lightDirection)
+{
+        shader->setVec3("viewPos", viewPos);
+        shader->setVec3("dirLight.direction", lightDirection);
+        shader->setVec3("dirLight.ambient", ambient);
+        shader->setVec3("dirLight.diffuse", diffuse);
+        shader->setVec3("dirLight.specular", specular);
+}
+
+PointLight::PointLight(glm::vec3 viewPos, glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, float constant, float linear, float quadratic)
+    :Lighting(viewPos, ambient, diffuse, specular),
+    constant(constant),linear(linear),quadratic(quadratic)
+{
+}
+
+void PointLight::apply(Shader* shader, unsigned int lightID, glm::vec3 lightPos)
+{
+    shader->setVec3("viewPos", viewPos);
+    shader->setVec3("pointLights[" + std::to_string(lightID) + "].position", lightPos);
+    shader->setVec3("pointLights[" + std::to_string(lightID) + "].ambient", ambient);
+    shader->setVec3("pointLights[" + std::to_string(lightID) + "].diffuse", diffuse);
+    shader->setVec3("pointLights[" + std::to_string(lightID) + "].specular", specular);
+    shader->setFloat("pointLights[" + std::to_string(lightID) + "].constant", constant);
+    shader->setFloat("pointLights[" + std::to_string(lightID) + "].linear", linear);
+    shader->setFloat("pointLights[" + std::to_string(lightID) + "].quadratic", quadratic);
+}
+
+SpotLight::SpotLight(glm::vec3 viewPos, glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, float constant, float linear, float quadratic, float cutOff, float outerCutOff)
+    :Lighting(viewPos, ambient, diffuse, specular),
+    constant(constant),linear(linear),quadratic(quadratic),cutOff(cutOff),outerCutOff(outerCutOff)
+{
+}
+
+void SpotLight::apply(Shader* shader, unsigned int lightID, glm::vec3 lightPos, glm::vec3 lightDirection)
+{
+    shader->setVec3("viewPos", viewPos);
+    shader->setVec3("spotLights[" + std::to_string(lightID) + "].position", lightPos);
+    shader->setVec3("spotLights[" + std::to_string(lightID) + "].direction", lightDirection);
+    shader->setVec3("spotLights[" + std::to_string(lightID) + "].ambient", ambient);
+    shader->setVec3("spotLights[" + std::to_string(lightID) + "].diffuse", diffuse);
+    shader->setVec3("spotLights[" + std::to_string(lightID) + "].specular", specular);
+    shader->setFloat("spotLights[" + std::to_string(lightID) + "].constant", constant);
+    shader->setFloat("spotLights[" + std::to_string(lightID) + "].linear", linear);
+    shader->setFloat("spotLights[" + std::to_string(lightID) + "].quadratic", quadratic);
+    shader->setFloat("spotLights[" + std::to_string(lightID) + "].cutOff", cutOff);
+    shader->setFloat("spotLights[" + std::to_string(lightID) + "].outerCutOff", outerCutOff);
+}
